9f983250b9b0d0ad0dd840f7db5211c40d6dfadb
Wiktorkulas51 <56845955+Wiktorkulas51@users.noreply.github.com>
2020-09-15 14:04:09 +0200

    everything

---
+++ node.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const upload = require("express-fileupload");
+
+app.use(express.static(path.join(__dirname, "src")));
+
+app.use(upload());
+
+app.post("/", (req, res) => {
+  const file = req.files.inpFile;
+  const fileName = file.name;
+
+  file.mv(`./src/uploads/${fileName}`, (err) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.redirect("http://127.0.0.1:3000/");
+      //send properly respons
+    }
+  });
+});
+
+const server = app.listen(3000, () => {
+  console.log("lis");
+});
