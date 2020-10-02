bd94b5c3828371c6e6ec291a7ab8c7fac83130f5
wiktorkulas51 <wiktorkulas51@gmail.com>
2020-10-02 16:42:45 +0200

    refactoring

--- root/index.js
+++ root/index.js
@@ -1,5 +1,5 @@
 // const btn = document.querySelector(".btn");
-const inpFile = document.querySelector(".real-file");
+const inputeFile = document.querySelector(".real-file");
 const myForm = document.querySelector(".myForm");
 const btnPrevious = document.querySelector(".previous");
 const btnNext = document.querySelector(".next");
@@ -10,8 +10,8 @@ const guide = document.querySelector(".guide");
 const labelForFiles = document.querySelector(".inpute-label");
 
 labelForFiles.onclick = () => {
-  inpFile.click();
-  if (inpFile.value != inpFile.value) {
+  inputeFile.click();
+  if (inputeFile.value != inputeFile.value) {
     return;
   } else {
     labelForFiles.innerHTML = "File has been choose";
@@ -26,7 +26,7 @@ const myState = {
   fileName: "./example2.pdf",
 };
 
-inpFile.onchange = (e) => {
+inputeFile.onchange = (e) => {
   let file = e.target.files;
   console.log(file);
   const fileName = `./${file[0].name}`;
@@ -37,43 +37,26 @@ inpFile.onchange = (e) => {
 
 const localFileNameDate = localStorage.getItem("myKey");
 
-if (!localFileNameDate) {
-  const pdfJsLib = pdfjsLib.getDocument(myState.fileName);
-  pdfJsLib.promise.then((pdf) => {
-    const viewer = document.querySelector(".canvas_conteiner");
-    myState.pdf = pdf;
-    myState.fullPages = myState.pdf.numPages;
+const pdfJsLib = pdfjsLib.getDocument(
+  !localFileNameDate ? myState.fileName : localFileNameDate
+);
+pdfJsLib.promise.then((pdf) => {
+  const viewer = document.querySelector(".canvas_conteiner");
+  myState.pdf = pdf;
+  myState.fullPages = myState.pdf.numPages;
 
-    const maxPage = document.querySelector(".maxPage");
-    maxPage.innerHTML = `/${myState.fullPages}`;
+  const maxPage = document.querySelector(".maxPage");
+  maxPage.innerHTML = `/${myState.fullPages}`;
 
-    if (myState.fullPages >= 1) {
-      canvas = document.createElement("canvas");
-      canvas.className = "pdf_renderer";
-      viewer.appendChild(canvas);
-      render(myState.currentPage, canvas);
-    }
-  });
-} else {
-  const pdfJsLib = pdfjsLib.getDocument(localFileNameDate);
-  pdfJsLib.promise.then((pdf) => {
-    const viewer = document.querySelector(".canvas_conteiner");
-    myState.pdf = pdf;
-    myState.fullPages = myState.pdf.numPages;
+  if (myState.fullPages >= 1) {
+    canvas = document.createElement("canvas");
+    canvas.className = "pdf_renderer";
+    viewer.appendChild(canvas);
+    render(myState.currentPage, canvas);
+  }
+});
 
-    const maxPage = document.querySelector(".maxPage");
-    maxPage.innerHTML = `/${myState.fullPages}`;
-
-    if (myState.fullPages >= 1) {
-      canvas = document.createElement("canvas");
-      canvas.className = "pdf_renderer";
-      viewer.appendChild(canvas);
-      render(myState.currentPage, canvas);
-    }
-  });
-}
-
-async function render(pageNumber, canvas, scale = 1.2) {
+function render(pageNumber, canvas, scale = 1.2) {
   const PRINT_UNITS = 100 / 72;
   const ctx = canvas.getContext("2d");
 
@@ -137,63 +120,52 @@ btnPrevious.addEventListener("click", () => {
 });
 
 zoomIn.addEventListener("click", () => {
-  myState.zoom = myState.zoom + 0.2;
+  myState.zoom -= 0.1;
   render(myState.currentPage, canvas, myState.zoom);
 });
 
 zoomOut.addEventListener("click", () => {
-  if (myState.zoom != 1.2) {
-    myState.zoom = myState.zoom - 0.2;
+  if (myState.zoom <= 1.2) {
+    myState.zoom += 0.1;
   }
   render(myState.currentPage, canvas, myState.zoom);
 });
 
 inputeCrruentPage.addEventListener("keypress", (e) => {
-  myState.currentPage = inputeCrruentPage.value;
   const val = inputeCrruentPage.value;
+  myState.currentPage = val;
   e.keyCode == 13 ? render(parseInt(val), canvas) : console.log("");
 
   if (val >= myState.fullPages) {
-    alert(`The pdf has only ${myState.fullPages} pages`);
+    alert(`This pdf has only ${myState.fullPages} pages`);
   }
 });
 
 async function getPdfText() {
   let check = false;
   const arrayOfTheText = [];
-  if (!localFileNameDate) {
-    let doc = await pdfjsLib.getDocument(myState.fileName).promise;
-    let pageTexts = Array.from({ length: doc.numPages }, async (v, i) => {
-      return (await (await doc.getPage(i + 1)).getTextContent()).items.map(
-        (token) => token.str
-      );
-    });
 
-    const text = (await Promise.all(pageTexts)).join("");
-    arrayOfTheText.push(text);
-  } else {
-    let doc = await pdfjsLib.getDocument(localFileNameDate).promise;
-    let pageTexts = Array.from({ length: doc.numPages }, async (v, i) => {
-      return (await (await doc.getPage(i + 1)).getTextContent()).items.map(
-        (token) => token.str
-      );
-    });
+  let doc = await pdfjsLib.getDocument(
+    !localFileNameDate ? myState.fileName : localFileNameDate
+  ).promise;
+  let pageTexts = Array.from({ length: doc.numPages }, async (v, i) => {
+    return (await (await doc.getPage(i + 1)).getTextContent()).items.map(
+      (token) => token.str
+    );
+  });
 
-    const text = (await Promise.all(pageTexts)).join("");
-    arrayOfTheText.push(text);
-  }
+  const text = (await Promise.all(pageTexts)).join("");
+  arrayOfTheText.push(text);
 
   const stopButton = document.querySelector(".stop");
-
   stopButton.addEventListener("click", () => {
     check = true;
     responsiveVoice.pause();
   });
 
   const speakButton = document.querySelector(".speak");
-
   speakButton.addEventListener("click", () => {
-    arrayOfTheText.forEach(async (element) => {
+    arrayOfTheText.forEach((element) => {
       if (check) {
         responsiveVoice.resume();
       } else {
